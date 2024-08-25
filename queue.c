27fc9632b15c3bdeda45ff13d722d184d596ced8
riain <ryanftoal@gmail.com>
2024-08-25 15:33:46 +0100

    Updated CMake

---
+++ queue.c
@@ -0,0 +1,5 @@
+//
+// Created by riain on 25/08/24.
+//
+
+#include "queue.h"
---
+++ queue.h
@@ -0,0 +1,8 @@
+//
+// Created by riain on 25/08/24.
+//
+
+#ifndef CHEAP_QUEUE_H
+#define CHEAP_QUEUE_H
+
+#endif //CHEAP_QUEUE_H
