7be1ced8a44b485f531be9095faa213a41235856
riain <ryanftoal@gmail.com>
2024-08-25 18:07:20 +0100

    Updated CMake

--- src/deque.c
+++ src/deque.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <memory.h>
-#include "deque.h"
+#include "../deque.h"
 
 typedef struct DoubleEndedQueue
 {
@@ -23,12 +23,8 @@ Deque *create_deque(size_t size)
 {
     Deque *deque = malloc(sizeof(Deque));
 
-    deque->capacity = 0;
-    deque->nmemb = 0;
     deque->size = size;
-
-    deque->array = malloc(deque->capacity * size);
-    memset(deque->array, 0, deque->capacity * size);
+    clear(deque);
 
     return deque;
 }
@@ -58,6 +54,28 @@ void push_back(Deque *deque, void* item)
     memcpy(deque->array + (deque->nmemb * deque->size), item, deque->size);
     deque->nmemb++;
 }
+void insert(Deque *deque, void *const item, const size_t index)
+{
+    if (index <= deque->nmemb)
+    {
+        if (deque->nmemb >= deque->capacity)
+            resize(deque);
+
+        if (index == deque->nmemb)
+            push_back(deque, item);
+        else
+        {
+            memmove(
+                deque->array + (index + 1) * deque->size,
+                deque->array + index * deque->size,
+                (deque->nmemb - index) * deque->size
+            );
+            memcpy(deque->array + index * deque->size, item, deque->size);
+            deque->nmemb++;
+        }
+    }
+}
+
 void *front(Deque *deque)
 {
     return (deque->nmemb) ? deque->array : NULL;
@@ -66,6 +84,7 @@ void *back(Deque *deque)
 {
     return (deque->nmemb) ? deque->array + (deque->nmemb - 1) * deque->size : NULL;
 }
+
 void pop_front(Deque *deque)
 {
     if (deque->nmemb)
@@ -78,6 +97,33 @@ void pop_back(Deque *deque)
 {
     if (deque->nmemb)
         deque->nmemb--;
+}
+size_t erase(Deque *deque, size_t index)
+{
+    if (deque->nmemb)
+    {
+        if (index == deque->nmemb - 1)
+        {
+            pop_back(deque);
+            index--;
+        }
+        else
+        {
+            memmove(deque->array + index, deque->array + index + 1, (deque->nmemb - index) * deque->size);
+            deque->nmemb--;
+            index--;
+        }
+    }
+
+    return index;
+}
+void clear(Deque *deque)
+{
+    deque->capacity = 0;
+    deque->nmemb = 0;
+
+    deque->array = malloc(deque->capacity * deque->size);
+    memset(deque->array, 0, deque->capacity * deque->size);
 }
 
 bool empty(Deque *deque)
