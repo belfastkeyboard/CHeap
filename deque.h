9be8bcd030c4595e87d4f8bf374e59ba8d6a31be
riain <ryanftoal@gmail.com>
2024-08-25 18:07:47 +0100

    Added additional functons for deque

--- deque.h
+++ deque.h
@@ -11,10 +11,15 @@ void destroy_deque(Deque **deque);
 
 void push_front(Deque *deque, void* item);
 void push_back(Deque *deque, void* item);
+void insert(Deque *deque, void *item, size_t index);
+
 void *front(Deque *deque);
 void *back(Deque *deque);
+
 void pop_front(Deque *deque);
 void pop_back(Deque *deque);
+size_t erase(Deque *deque, size_t index);
+void clear(Deque *deque);
 
 bool empty(Deque *deque);
 size_t size(Deque *deque);
