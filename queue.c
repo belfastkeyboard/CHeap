a3e7b0675917c0f04e8fcb0b253e82a38cf58c01
riain <ryanftoal@gmail.com>
2024-08-25 15:33:59 +0100

    Added queue to lib

--- queue.c
+++ queue.c
@@ -1,5 +1,63 @@
-//
-// Created by riain on 25/08/24.
-//
-
+#include <stdlib.h>
+#include <memory.h>
 #include "queue.h"
+
+typedef struct Queue
+{
+    void *array;
+    size_t capacity;
+    size_t nmemb;
+    size_t size;
+} Queue;
+
+Queue *create_pqueue(size_t capacity, size_t size)
+{
+    Queue *queue = malloc(sizeof(Queue));
+
+    queue->capacity = capacity;
+    queue->nmemb = 0;
+    queue->size = size;
+
+    queue->array = malloc(capacity * size);
+    memset(queue->array, 0, capacity * size);
+
+    return queue;
+}
+void destroy_pqueue(Queue **queue)
+{
+    if ((*queue)->array)
+        free((*queue)->array);
+
+    free(*queue);
+    *queue = NULL;
+}
+
+void push(Queue *queue, void* item)
+{
+    if (queue->nmemb < queue->capacity)
+    {
+        memcpy(queue->array + (queue->nmemb * queue->size), item, queue->size);
+        queue->nmemb++;
+    }
+}
+void *front(Queue *queue)
+{
+    return (queue->nmemb) ? queue->array : NULL;
+}
+void pop(Queue *queue)
+{
+    if (queue->nmemb)
+    {
+        memmove(queue->array, queue->array + queue->size, (queue->nmemb - 1) * queue->size);
+        queue->nmemb--;
+    }
+}
+
+bool empty(Queue *queue)
+{
+    return queue->nmemb == 0;
+}
+size_t size(Queue *queue)
+{
+    return queue->nmemb;
+}
--- queue.h
+++ queue.h
@@ -1,8 +1,17 @@
-//
-// Created by riain on 25/08/24.
-//
+#pragma once
 
-#ifndef CHEAP_QUEUE_H
-#define CHEAP_QUEUE_H
+#include <stdbool.h>
+#include <stddef.h>
 
-#endif //CHEAP_QUEUE_H
+typedef struct Queue Queue;
+
+__attribute__((warn_unused_result))
+Queue *create_pqueue(size_t capacity, size_t size);
+void destroy_pqueue(Queue **queue);
+
+void push(Queue *queue, void* item);
+void *front(Queue *queue);
+void pop(Queue *queue);
+
+bool empty(Queue *queue);
+size_t size(Queue *queue);
