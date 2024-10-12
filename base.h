3c0938238c640fd8d539ca196addb1a0d0a0b040
riain <ryanftoal@gmail.com>
2024-10-12 03:28:49 +0100

    Removed mempool funcs to mpool.c, some re-works

--- base.h
+++ base.h
@@ -2,22 +2,21 @@
 
 #include <stdbool.h>
 #include <stddef.h>
+#include "arena.h"
 
-#define SEQ_CONTAINER_INIT(cntr) cntr->nmemb = 0; cntr->capacity = 0; cntr->size = size; cntr->array = NULL;
+#define EMPTY 0
+#define UNSET EMPTY
+
+#define SEQUENTIAL_GROWTH 2
+#define SEQUENTIAL_INIT 1
+
+#define ARENA_CONTAINER_INIT_COUNT 8
 
 void *memory_allocate_container(size_t size);
-void memory_free_buffer(void *buffer);
-void memory_free_container(void **container);
+void memory_free_mempool(void **buffer);
+void memory_free_container_mempool(void **container, void *array);
+void memory_free_container_arena(void **container, Arena *arena);
 
-bool container_empty(size_t nmemb);
-size_t container_size(size_t nmemb);
-size_t container_capacity(size_t capacity);
-
-__attribute__((warn_unused_result()))
-void *sequential_realloc(void *array, size_t capacity, size_t size);
-__attribute__((warn_unused_result()))
-void *sequential_resize(void *array, size_t *capacity, size_t size);
-void sequential_insert(void *array, size_t index, void *item, size_t nmemb, size_t size);
-size_t sequential_remove(void *array, size_t index, size_t nmemb, size_t size);
-void sequential_clear(void *array, size_t capacity, size_t size);
-void *sequential_index_access(void *array, size_t index, size_t size);
+bool generic_empty(size_t nmemb);
+size_t generic_size(size_t nmemb);
+size_t generic_capacity(size_t capacity);
