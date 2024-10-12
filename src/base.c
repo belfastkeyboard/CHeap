e73a4bf86e9a8e7bca16ab4afffeecfecfc2c6d7
riain <ryanftoal@gmail.com>
2024-10-12 03:30:15 +0100

    Removed mempool funcs to mpoo.c

--- src/base.c
+++ src/base.c
@@ -2,88 +2,51 @@
 #include <malloc.h>
 #include <memory.h>
 #include <stdbool.h>
+#include "../arena.h"
 #include "../base.h"
-
-#define EMPTY 0
-#define UNSET EMPTY
-
-#define SEQUENTIAL_GROWTH 2
-#define SEQUENTIAL_INIT 1
 
 void *memory_allocate_container(size_t size)
 {
-    void *ptr = malloc(size);
+    void *ptr = calloc(1, size);
 
     assert(ptr);
 
     return ptr;
 }
-void memory_free_buffer(void *buffer)
+void memory_free_mempool(void **buffer)
 {
-    if (buffer)
-        free(buffer);
+    if (*buffer)
+        free(*buffer);
+    *buffer = NULL;
 }
-void memory_free_container(void **container)
+void memory_free_container_mempool(void **container, void *array)
 {
+    assert(*container);
+
+    memory_free_mempool(&array);
+
+    free(*container);
+    *container = NULL;
+}
+void memory_free_container_arena(void **container, Arena *arena)
+{
+    assert(*container);
+
+    destroy_arena(&arena);
+
     free(*container);
     *container = NULL;
 }
 
-bool container_empty(size_t nmemb)
+bool generic_empty(size_t nmemb)
 {
     return (nmemb == EMPTY);
 }
-size_t container_size(size_t nmemb)
+size_t generic_size(size_t nmemb)
 {
     return nmemb;
 }
-size_t container_capacity(size_t capacity)
+size_t generic_capacity(size_t capacity)
 {
     return capacity;
-}
-
-void *sequential_realloc(void *array, size_t capacity, const size_t size)
-{
-    void *tmp = realloc(array, size * capacity);
-
-    assert(tmp);
-
-    return tmp;
-}
-void *sequential_resize(void *array, size_t *capacity, const size_t size)
-{
-    *capacity = (*capacity > EMPTY) ? *capacity * SEQUENTIAL_GROWTH : SEQUENTIAL_INIT;
-
-    return sequential_realloc(array, *capacity, size);
-}
-void sequential_insert(void *array, size_t index, void *item, size_t nmemb, size_t size)
-{
-    if (index < nmemb)
-        memmove(array + (index + 1) * size,array + index * size,(nmemb - index) * size);
-
-    memcpy(array + index * size, item, size);
-}
-size_t sequential_remove(void *array, size_t index, size_t nmemb, size_t size)
-{
-    if (index < nmemb - 1)
-        memmove(array + index * size, array + (index + 1) * size, (nmemb - index) * size);
-
-    return index - 1;
-}
-void sequential_clear(void *array, size_t capacity, size_t size)
-{
-    memset(array, UNSET, capacity * size);
-}
-void *sequential_index_access(void *array, size_t index, size_t size)
-{
-    return array + index * size;
-}
-
-void no_iterator_push_back(void **array, void *const item, size_t *nmemb, size_t *capacity, const size_t size)
-{
-    if (*nmemb >= *capacity)
-        *array = sequential_resize(*array, capacity, size);
-
-    sequential_insert(array, *nmemb, item, *nmemb, size);
-    (*nmemb)++;
 }
