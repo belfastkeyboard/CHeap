028c9dd47e753375870cc7babc7be2bb86a32459
riain <ryanftoal@gmail.com>
2024-10-12 03:21:23 +0100

    Removed global arena type, added error reporting, fixed bugs

--- arena.h
+++ arena.h
@@ -2,24 +2,30 @@
 
 #include <stddef.h>
 
+#define ARENA_NO_ERROR            0x0
+#define ARENA_REQUEST_OVERFLOW    0x1
+#define ARENA_INSUFFICIENT_MEMORY 0x2
+
 typedef struct Arena Arena;
 
 typedef enum
 {
     ARENA_STATIC,
-    ARENA_DYNAMIC,
-    ARENA_GLOBAL
+    ARENA_DYNAMIC
 } ArenaType;
 
 __attribute__((warn_unused_result))
-Arena *create_arena(size_t size, ArenaType type);
+Arena *create_arena(size_t nmemb, size_t size, ArenaType type);
 void destroy_arena(Arena **arena);
-void clear_arena(Arena *arena);
+
 __attribute__((warn_unused_result))
 void *alloc_arena(Arena *arena, size_t size);
 __attribute__((warn_unused_result))
 void *calloc_arena(Arena *arena, size_t size);
+
 void free_arena(Arena *arena, void *ptr, size_t size);
+void clear_arena(Arena *arena);
 
 size_t memory_remaining_arena(const Arena *arena);
 ArenaType get_type_arena(const Arena *arena);
+int get_arena_error_code(void);
