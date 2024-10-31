9a29267e17ac4b423dda6bcb81928f2b27a2d62c
riain <ryanftoal@gmail.com>
2024-10-31 15:24:19 +0000

    Added debug allocators to track memory allocation/de-allocation

---
+++ debug_malloc.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void *debug_malloc(size_t size, const char *text);
+void *debug_realloc(void *data, size_t size, const char *text);
+void debug_free(void *ptr, size_t size, const char *text);
