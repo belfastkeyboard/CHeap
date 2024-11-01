3da97af69327c1f68d0de5fe1bab68877366150c
riain <ryanftoal@gmail.com>
2024-11-01 05:33:03 +0000

    Updated free hash container function to free values from hash table

--- base.h
+++ base.h
@@ -16,7 +16,7 @@ void *memory_allocate_container(size_t size);
 void memory_free_buffer(void **buffer);
 void memory_free_container_mempool(void **container, void *array);
 void memory_free_container_arena(void **container, Arena *arena);
-void memory_free_container_hash(void **container, void *buckets, void *keys);
+void memory_free_container_hash(void **container, void *buckets, void *keys, void *values);
 
 bool generic_empty(size_t nmemb);
 size_t generic_size(size_t nmemb);
