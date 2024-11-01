19facdb92edcdd70586a91a33834b3e883d12dfe
riain <ryanftoal@gmail.com>
2024-11-01 05:36:06 +0000

    Added hash table to generic macros

--- cheap.h
+++ cheap.h
@@ -5,30 +5,35 @@
 #include "list.h"
 #include "flist.h"
 #include "hash_set.h"
+#include "hash_table.h"
 #include "pqueue.h"
 #include "queue.h"
 #include "stack.h"
 
 // constructors / destructors
+
+// TODO: possibly use macro to avoid having to call create on hash table with sizeof for value type
 #define create(container_type, storage_type, ...) _Generic((container_type), \
-    Array*:   create_array,                                                  \
-    Deque*:   create_deque,                                                  \
-    FList*:   create_forward_list,                                           \
-    HashSet*: create_hash_set,                                               \
-    List*:    create_list,                                                   \
-    PQueue*:  create_pqueue,                                                 \
-    Queue*:   create_queue,                                                  \
-    Stack*:   create_stack                                                   \
+    Array*:     create_array,                                                \
+    Deque*:     create_deque,                                                \
+    FList*:     create_forward_list,                                         \
+    HashSet*:   create_hash_set,                                             \
+    HashTable*: create_hash_table,                                           \
+    List*:      create_list,                                                 \
+    PQueue*:    create_pqueue,                                               \
+    Queue*:     create_queue,                                                \
+    Stack*:     create_stack                                                 \
 )(sizeof(storage_type), ##__VA_ARGS__)
 #define destroy(container) _Generic((container), \
-    Array**:   destroy_array,                    \
-    Deque**:   destroy_deque,                    \
-    FList**:   destroy_forward_list,             \
-    HashSet**: destroy_hash_set,                 \
-    List**:    destroy_list,                     \
-    PQueue**:  destroy_pqueue,                   \
-    Queue**:   destroy_queue,                    \
-    Stack**:   destroy_stack                     \
+    Array**:     destroy_array,                  \
+    Deque**:     destroy_deque,                  \
+    FList**:     destroy_forward_list,           \
+    HashSet**:   destroy_hash_set,               \
+    HashTable**: destroy_hash_table,             \
+    List**:      destroy_list,                   \
+    PQueue**:    destroy_pqueue,                 \
+    Queue**:     destroy_queue,                  \
+    Stack**:     destroy_stack                   \
 )(container)
 
 // sequence containers
@@ -43,10 +48,11 @@
     List*:  push_back_list                                \
 )(container, value)
 #define insert(container, value, ...) _Generic((container), \
-    Array*:   insert_array,                                 \
-    Deque*:   insert_deque,                                 \
-    HashSet*: insert_hash_set,                              \
-    List*:    insert_list                                   \
+    Array*:     insert_array,                               \
+    Deque*:     insert_deque,                               \
+    HashSet*:   insert_hash_set,                            \
+    HashTable*: insert_hash_table,                          \
+    List*:      insert_list                                 \
 )(container, value, ##__VA_ARGS__)
 #define insert_after(container, value, index) _Generic((container), \
     FList*: insert_after_forward_list                               \
@@ -63,18 +69,20 @@
     List*:  pop_back_list                         \
 )(container)
 #define erase(container, index_or_value) _Generic((container), \
-    Array*:   erase_array,                            \
-    Deque*:   erase_deque,                            \
-    HashSet*: erase_hash_set                          \
+    Array*:     erase_array,                                   \
+    Deque*:     erase_deque,                                   \
+    HashSet*:   erase_hash_set,                                \
+    HashTable*: erase_hash_table                               \
 )(container, index_or_value)
 #define erase_after(container, index) _Generic((container), \
-    FList*: erase_after_forward_list                               \
+    FList*: erase_after_forward_list                        \
 )(container, index)
 #define clear(container) _Generic((container), \
-    Array*:   clear_array,                     \
-    Deque*:   clear_deque,                     \
-    FList*:   clear_forward_list,              \
-    HashSet*: clear_hash_set                   \
+    Array*:     clear_array,                   \
+    Deque*:     clear_deque,                   \
+    FList*:     clear_forward_list,            \
+    HashSet*:   clear_hash_set,                \
+    HashTable*: clear_hash_table               \
 )(container)
 
 #define at(type, container, index) *(type*) _Generic((container), \
@@ -95,38 +103,30 @@
 )(container)
 
 #define empty(container) _Generic((container), \
-    Array*:   empty_array,                     \
-    Deque*:   empty_deque,                     \
-    FList*:   empty_forward_list,              \
-    HashSet*: empty_hash_set,                  \
-    List*:    empty_list,                      \
-    PQueue*:  empty_pqueue,                    \
-    Queue*:   empty_queue,                     \
-    Stack*:   empty_stack                      \
+    Array*:     empty_array,                   \
+    Deque*:     empty_deque,                   \
+    FList*:     empty_forward_list,            \
+    HashSet*:   empty_hash_set,                \
+    HashTable*: empty_hash_table,              \
+    List*:      empty_list,                    \
+    PQueue*:    empty_pqueue,                  \
+    Queue*:     empty_queue,                   \
+    Stack*:     empty_stack                    \
 )(container)
 #define size(container) _Generic((container), \
-    Array*:   size_array,                     \
-    Deque*:   size_deque,                     \
-    FList*:   size_forward_list,              \
-    HashSet*: size_hash_set,                  \
-    List*:    size_list,                      \
-    PQueue*:  size_pqueue,                    \
-    Queue*:   size_queue,                     \
-    Stack*:   size_stack                      \
+    Array*:     size_array,                   \
+    Deque*:     size_deque,                   \
+    FList*:     size_forward_list,            \
+    HashSet*:   size_hash_set,                \
+    HashTable*: size_hash_table,              \
+    List*:      size_list,                    \
+    PQueue*:    size_pqueue,                  \
+    Queue*:     size_queue,                   \
+    Stack*:     size_stack                    \
 )(container)
 #define capacity(container) _Generic((container), \
     Array*: capacity_array                        \
 )(container)
-
-#define count(container, value) _Generic((container), \
-    HashSet*: count_hash_set                          \
-)(container, value)
-#define find(container, value) _Generic((container), \
-    HashSet*: find_hash_set                          \
-)(container, value)
-#define contains(container, value) _Generic((container), \
-    HashSet*: contains_hash_set                          \
-)(container, value)
 
 #define reserve(container, new_cap) _Generic((container), \
     Array*: reserve_array                                 \
@@ -140,6 +140,18 @@
 // associative containers
 
 // unordered associative containers
+#define count(container, value) _Generic((container), \
+    HashSet*:   count_hash_set                        \
+    HashTable*: count_hash_table                      \
+)(container, value)
+#define find(container, value) _Generic((container), \
+    HashSet*:   find_hash_set,                       \
+    HashTable*: find_hash_table                      \
+)(container, value)
+#define contains(container, value) _Generic((container), \
+    HashSet*:   contains_hash_set                        \
+    HashTable*: contains_hash_table                      \
+)(container, value)
 
 // container adaptors
 #define push(container, value) _Generic((container), \
