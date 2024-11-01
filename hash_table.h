def89a758f7e9d3d8bb1d5b539b1d18b0aba727b
riain <ryanftoal@gmail.com>
2024-11-01 05:35:39 +0000

    Complete refactor to accept generic internal impl

--- hash_table.h
+++ hash_table.h
@@ -3,22 +3,22 @@
 #include <stdbool.h>
 #include <stddef.h>
 
-typedef struct Table Table;
-typedef int (*KeyComp)(const void *a, const void *b);
-typedef bool (*ValueComp)(const void *a, const void *b);
+typedef struct HashTable HashTable;
+typedef int (*KComp)(const void *a, const void *b);
+typedef bool (*VComp)(const void *a, const void *b);
 
 __attribute__((warn_unused_result))
-Table *create_hash_table(size_t key_size, size_t value_size, KeyComp kc, ValueComp vc);
-void destroy_hash_table(Table **table);
+HashTable *create_hash_table(size_t key_size, size_t value_size, KComp kc);
+void destroy_hash_table(HashTable **table);
 
-void insert_hash_table(Table *table, void *key, void *value);
+void insert_hash_table(HashTable *table, void *key, void *value);
 
-size_t count_hash_table(Table *table, void *key);
-void *find_hash_table(Table *table, void *key);
-bool contains_hash_table(Table *table, void *key);
+size_t count_hash_table(HashTable *table, void *key);
+void *find_hash_table(HashTable *table, void *key);
+bool contains_hash_table(HashTable *table, void *key);
 
-void erase_hash_table(Table *table, void *key);
-void clear_hash_table(Table *table);
+void erase_hash_table(HashTable *table, void *key);
+void clear_hash_table(HashTable *table);
 
-bool empty_hash_table(Table *table);
-size_t size_hash_table(Table *table);
+bool empty_hash_table(HashTable *table);
+size_t size_hash_table(HashTable *table);
