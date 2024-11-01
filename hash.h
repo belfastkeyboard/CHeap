b5f4713bc03c5bb96ec8ed5a0a326fc2f4264c3e
riain <ryanftoal@gmail.com>
2024-11-01 05:34:15 +0000

    Updated function parameters to accept values, moved preproc statements to source file

--- hash.h
+++ hash.h
@@ -1,29 +1,20 @@
 #pragma once
 
 #include <stdbool.h>
+#include <stddef.h>
 
 struct Bucket;
 typedef unsigned long hash_t;
 
-#define GROW_FACTOR        2.0f
-#define SHRINK_FACTOR      0.5f
-#define LF_UPPER_THRESHOLD 0.75f
-#define LF_LOWER_THRESHOLD 0.1f
-#define TABLE_MIN          8
-
-#define UNSET     (-1)
-#define INVALID   UNSET
-#define NOT_FOUND INVALID
-
-typedef int (*KeyComp)(const void *a, const void *b);
+typedef int (*KComp)(const void *a, const void *b);
 
 hash_t djb2(const void *item, size_t size);
 size_t get_index(hash_t hash, size_t capacity);
 
-void hash_insert(KeyComp k_comp, struct Bucket **buckets, void **keys, size_t *capacity, size_t k_size, size_t *nmemb, void *key);
-void hash_erase(KeyComp k_comp, struct Bucket *buckets, void *keys, size_t *capacity, size_t k_size, void *key, size_t *nmemb);
-void hash_clear(struct Bucket **buckets, void **keys, size_t *capacity, size_t *nmemb);
+void hash_insert(struct Bucket **buckets, void **keys, void **values, size_t k_size, size_t v_size, KComp k_comp, size_t *nmemb, size_t *capacity, void *key, void *value);
+void hash_erase(struct Bucket **buckets, void **keys, void **values, size_t k_size, size_t v_size, KComp k_comp, size_t *nmemb, size_t *capacity, void *key);
+void hash_clear(struct Bucket **buckets, void **keys, void **values, size_t *nmemb, size_t *capacity);
 
-size_t hash_count(KeyComp k_comp, struct Bucket *buckets, void *keys, size_t capacity, size_t k_size, void *key);
-void *hash_find(KeyComp k_comp, struct Bucket *buckets, void *keys, size_t capacity, size_t k_size, void *key);
-bool hash_contains(KeyComp k_comp, struct Bucket *buckets, void *keys, size_t capacity, size_t k_size, void *key);
+size_t hash_count(struct Bucket *buckets, void *keys, size_t k_size, KComp k_comp, size_t capacity, void *key);
+void *hash_find(struct Bucket *buckets, void *keys, void *values, size_t k_size, size_t v_size, KComp k_comp, size_t capacity, size_t nmemb, void *key);
+bool hash_contains(struct Bucket *buckets, void *keys, size_t k_size, KComp k_comp, size_t capacity, void *key);
