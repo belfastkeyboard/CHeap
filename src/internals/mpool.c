69e351b5077efaacd5e36a896a2c11fabfa82ddb
riain <ryanftoal@gmail.com>
2024-11-12 21:56:25 +0000

    Added assert and generic access functions to internal mempool access

--- src/internals/mpool.c
+++ src/internals/mpool.c
@@ -26,11 +26,13 @@ void *mempool_resize(void *array,
 }
 
 void mempool_insert(void *array,
-                    size_t index,
+                    const size_t index,
                     void *value,
-                    size_t nmemb,
-                    size_t size)
+                    const size_t nmemb,
+                    const size_t size)
 {
+    assert(index < nmemb + 1);
+
     if (index < nmemb)
     {
         memmove(array + (index + 1) * size,
@@ -44,34 +46,37 @@ void mempool_insert(void *array,
 }
 
 size_t mempool_remove(void *array,
-                      size_t index,
-                      size_t nmemb,
-                      size_t size)
+                      const size_t index,
+                      const size_t nmemb,
+                      const size_t size)
 {
-    if (index < nmemb - 1)
-    {
-        memmove(array + index * size,
-                array + (index + 1) * size,
-                (nmemb - index) * size);
-    }
+    assert(array && index < nmemb);
+
+    memmove(array + index * size,
+            array + (index + 1) * size,
+            (nmemb - index) * size);
 
     return index - 1;
 }
 
 // TODO: is this operation necessary or is setting nmemb to 0 enough?
 void mempool_clear(void *array,
-                   size_t capacity,
-                   size_t size)
+                   const size_t capacity,
+                   const size_t size)
 {
+    assert(array);
+
     memset(array,
            UNSET,
            capacity * size);
 }
 
 void *mempool_random_access(void *array,
-                            size_t index,
-                            size_t size)
+                            const size_t index,
+                            const size_t size)
 {
+    assert(array);
+
     return array + index * size;
 }
 
@@ -80,7 +85,7 @@ void generic_mempool_push_back(void **array,
                                void *value,
                                size_t *capacity,
                                size_t *nmemb,
-                               size_t size)
+                               const size_t size)
 {
     generic_mempool_insert(array,
                            value,
@@ -94,7 +99,7 @@ void generic_mempool_push_front(void **array,
                                 void *value,
                                 size_t *capacity,
                                 size_t *nmemb,
-                                size_t size)
+                                const size_t size)
 {
     generic_mempool_insert(array,
                            value,
@@ -106,10 +111,10 @@ void generic_mempool_push_front(void **array,
 
 void generic_mempool_insert(void **array,
                             void *value,
-                            size_t index,
+                            const size_t index,
                             size_t *capacity,
                             size_t *nmemb,
-                            size_t size)
+                            const size_t size)
 {
     if (*nmemb >= *capacity)
     {
@@ -137,7 +142,7 @@ void generic_mempool_pop_back(size_t *nmemb)
 
 void generic_mempool_pop_front(void **array,
                                size_t *nmemb,
-                               size_t size)
+                               const size_t size)
 {
     assert(*nmemb);
 
@@ -152,8 +157,10 @@ void generic_mempool_pop_front(void **array,
 size_t generic_mempool_erase(void **array,
                              size_t index,
                              size_t *nmemb,
-                             size_t size)
+                             const size_t size)
 {
+    assert(*array && index < *nmemb);
+
     index = mempool_remove(*array,
                            index,
                            *nmemb,
@@ -165,9 +172,9 @@ size_t generic_mempool_erase(void **array,
 }
 
 void generic_mempool_clear(void **array,
-                           size_t capacity,
+                           const size_t capacity,
                            size_t *nmemb,
-                           size_t size)
+                           const size_t size)
 {
     mempool_clear(*array,
                   capacity,
@@ -177,27 +184,59 @@ void generic_mempool_clear(void **array,
 }
 
 
+
+void *generic_mempool_access_front(void *array,
+                                   size_t size)
+{
+    return mempool_random_access(array,
+                                 0,
+                                 size);
+}
+
+void *generic_mempool_access_back(void *array,
+                                  size_t size,
+                                  size_t nmemb)
+{
+    return mempool_random_access(array,
+                                 nmemb - 1,
+                                 size);
+}
+
+void *generic_mempool_random_access(void *array,
+                                    const size_t index,
+                                    const size_t nmemb,
+                                    const size_t size)
+{
+    assert(index < nmemb);
+
+    return mempool_random_access(array,
+                                 index,
+                                 size);
+}
+
+
 void generic_mempool_reserve(void **array,
                              size_t *capacity,
-                             size_t size,
-                             size_t new_cap)
+                             const size_t size,
+                             const size_t new_cap)
 {
     if (new_cap > *capacity)
     {
         *array = mempool_realloc(*array,
                                  new_cap * size);
+
         *capacity = new_cap;
     }
 }
 
 void generic_mempool_shrink_to_fit(void **array,
                                    size_t *capacity,
-                                   const size_t *nmemb,
-                                   size_t size)
+                                   const size_t nmemb,
+                                   const size_t size)
 {
-    if (*nmemb < *capacity)
+    if (nmemb < *capacity)
     {
-        *capacity = *nmemb;
+        *capacity = nmemb;
 
         *array = mempool_realloc(*array,
                                  *capacity * size);
