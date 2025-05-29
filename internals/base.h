465e64525c79184c3afe51d2cc73020fc76e139d
riain <ryanftoal@gmail.com>
2025-05-29 14:56:32 +0100

    Formatting

--- internals/base.h
+++ internals/base.h
@@ -9,16 +9,13 @@
 #define SEQUENTIAL_GROWTH 2
 #define SEQUENTIAL_INIT 1
 
-void *memory_allocate_container(size_t size);
+#define ALLOC __attribute__((warn_unused_result))
+
+ALLOC void *memory_allocate_container(size_t size);
 
 void memory_free_buffer(void **buffer);
-
-void memory_free_container_generic(void **container,
-                                   void *array);
-
+void memory_free_container_generic(void **container, void *array);
 
 bool generic_empty(size_t nmemb);
-
 size_t generic_size(size_t nmemb);
-
 size_t generic_capacity(size_t capacity);
