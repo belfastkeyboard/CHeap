8716e93058df67cb08ab0ae142f039b6a7c9bcd1
riain <ryanftoal@gmail.com>
2025-06-03 21:14:29 +0100

    Formatting

--- internals/cassert.h
+++ internals/cassert.h
@@ -7,18 +7,19 @@
 #define CHEAP_ASSERT(expr, msg) ((void)0)
 #else
 #define CHEAP_ASSERT(expr, msg)                                                \
-	do {                                                                       \
-		if (!(expr)) {                                                         \
+	do                                                                         \
+	{                                                                          \
+		if (!(expr))                                                           \
+		{                                                                      \
 			fprintf(stderr, "%s\n", msg);                                      \
-                                                                               \
 			fprintf(stderr,                                                    \
 			        "CHeap: %s:%d: %s: Assertion '%s' failed.\n",              \
 			        __FILE__,                                                  \
 			        __LINE__,                                                  \
 			        __extension__ __PRETTY_FUNCTION__,                         \
 			        #expr);                                                    \
-                                                                               \
 			abort();                                                           \
 		}                                                                      \
-	} while (0)
+	}                                                                          \
+	while (0)
 #endif
